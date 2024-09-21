d2bff2a17f0e3027116545e93faa039607b20020
Chaudhary Muhammad Irfan <chirfan2002muhammad@gmail.com>
2024-09-21 22:32:18 +0500

    Adding the html , css and js files having the functionlity to change the background color on the basic of toggle button

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggleSwitch = document.querySelector('.input');
+const body = document.body;
+toggleSwitch.addEventListener('change' , () => {
+  if (toggleSwitch.checked) {
+    body.classList.remove('light-mode');
+    body.classList.add('dark-mode');
+  } else {
+    body.classList.remove('dark-mode');
+    body.classList.add('light-mode');
+  }
+});
